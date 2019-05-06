6b01234284e00d68da42a198b6a67365a104e1aa
liuwentao01 <liuwentao01@baidu.com>
2019-05-06 11:02:29 +0800

    kruskal

---
+++ greedy/kruskal/kruskal.go
@@ -0,0 +1,65 @@
+package kruskal
+
+import (
+	"algo/graph"
+	"sort"
+)
+
+type Edge struct {
+	U int // from
+	V int // to
+	W int // weight
+}
+
+type EdgeSet []Edge
+
+func (s EdgeSet) Len() int {
+	return len(s)
+}
+func (s EdgeSet) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s EdgeSet) Less(i, j int) bool {
+	return s[i].W < s[j].W
+}
+
+func ffind(fa []int, x int) int {
+	if fa[x] == x {
+		return x
+	} else {
+		return ffind(fa, fa[x])
+	}
+}
+
+// 单向图
+func Kruskal(g *graph.Graph) EdgeSet {
+	es := EdgeSet{}
+	for i, _ := range g.Adj {
+		cur := g.Adj[i].Iter()
+		for cur != nil {
+			edge := Edge{i, cur.Key.(int), cur.Data.(int)}
+			es = append(es, edge)
+			cur = g.Adj[i].Iter()
+		}
+		g.Adj[i].ResetIter()
+	}
+	sort.Sort(es)
+
+	result := EdgeSet{}
+	fa := make([]int, g.V)
+	for i, _ := range fa {
+		fa[i] = i
+	}
+	for _, edge := range es {
+		u := edge.U
+		v := edge.V
+		if ffind(fa, u) != ffind(fa, v) {
+			result = append(result, edge)
+			if len(es) == g.V-1 {
+				break
+			}
+			fa[ffind(fa, v)] = ffind(fa, u)
+		}
+	}
+	return result
+}
